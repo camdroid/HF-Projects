c9613b3df95fd0a18600c89f4066fdd1a272d36e
Cam <camherringshaw@gmail.com>
2015-07-20 14:51:13 -0400

    Photo grid works with angular

--- week3/angular-directives/script.js
+++ week3/angular-directives/script.js
@@ -1,26 +1,62 @@
-var app = angular.module('photo-grid-app', []);
+var app = angular.module('myapp', []);
 
 app.controller('MainCtrl', function($scope) {
-	$scope.image_src = "res/images/michigan.jpg";
-	$scope.image_cap = "Graduated from UMich";
-});
+	$scope.name = 'Bob';
 
-app.directive('photo-box',function(){
+	$scope.my_images = [{
+		src: 'michigan.jpg',
+		cap: 'Graduated from UMich'
+	}, {
+		src: 'computer-security.jpg',
+		cap: 'Focused on Computer Security'
+	}, {
+		src: 'duo.png',
+		cap: 'Working at Duo after bootcamp'
+	}, {
+		src: 'bell-tower.jpg',
+		cap: 'Overlooking Reykjavik'
+	}, {
+		src: 'plane.jpg',
+		cap: 'Crashed Navy plane near Skaftafell'
+	}, {
+		src: 'shimbashi.jpg',
+		cap: 'Shimbashi Station in Tokyo'
+	}, {
+		src: 'salmon-fishing.jpg',
+		cap: 'Salmon Fishing in Alaska'
+	}, {
+		src: 'marina.jpg',
+		cap: 'Celebrating a friend\'s engagement!'
+	}, {
+		src: 'han-river.jpg',
+		cap: 'Biking along the Han River in Seoul'
+	}];
+
+});
+app.directive('photoBox', function(){
 	return {
-		replace: true,
-		restrict: 'ECMA',
-		// templateUrl: 'partials/photo-box.html',
-		template: "<h1>Hello World</h1>",
+		replace:true,
+		scope: { 
+			source: '@',
+			cap: '@',
+			clicked: '=false'
+		},
+		restrict: 'AEC',
+		templateUrl: 'partials/photo-box.html',
 		link: function(scope, elem, attrs) {
-			// var img = document.createElement('img');
-			// img.src = 'res/images/michigan.jpg';
-	 
-			// // directives as comment
-			// if (element[0].nodeType === 8) {
-			// 	element.replaceWith(img);
-			// } else {
-			// 	element[0].appendChild(img);
-			// }
+			elem.bind('click', function() {
+				if(scope.clicked) {
+					elem.css('background-color', '#305275')
+					scope.clicked = false;
+				} else {
+					elem.css('background-color', 'gray');
+					scope.clicked = true;
+				}
+			});
+			elem.bind('mouseover', function() {
+				elem.css('cursor', 'pointer');
+				elem.css('border', 'none');
+			})
 		}
-	}
+	};
 });
